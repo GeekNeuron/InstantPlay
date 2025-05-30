83ec882b0166342f1d91dfe418da20f2ec8b9bff
Geek Neuron <geekneuron@gmail.com>
2025-05-31 02:15:18 +0330

    Create utils.js

---
+++ games/sudoku/js/utils.js
@@ -0,0 +1,8 @@
+// js/utils.js
+const Utils = (() => {
+    // function deepCopyArray(arr) {
+    //     return JSON.parse(JSON.stringify(arr)); // Simple deep copy for 2D arrays of primitives
+    // }
+    // return { deepCopyArray };
+    return {};
+})();
