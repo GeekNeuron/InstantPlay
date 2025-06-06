b334b678a5642ec58bddb04f0ca33c1f06a3f17d
Geek Neuron <geekneuron@gmail.com>
2025-06-06 19:08:05 +0330

    Update grid.js

--- games/2048/js/grid.js
+++ games/2048/js/grid.js
@@ -39,16 +39,22 @@ class Grid {
     }
 
     addRandomTile() {
-        const position = this.getRandomEmptyCellPosition();
-        if (position) {
-            const newTile = new Tile(this.gridContainerElement); 
-            // Pass true for isNewSpawn to trigger number pop animation in tile.setPosition
-            newTile.setPosition(position.r, position.c, this.size, this.gridContainerElement, true); 
-            this.tiles.push(newTile);
-            return newTile;
-        }
-        return null;
+    const position = this.getRandomEmptyCellPosition();
+    if (position) {
+        const newTile = new Tile(this.gridContainerElement);
+        newTile.setPosition(position.r, position.c, this.size, this.gridContainerElement, false);
+        this.tiles.push(newTile);
+        
+        setTimeout(() => {
+            if (newTile.triggerNumberPopAnimation) {
+                newTile.triggerNumberPopAnimation();
+            }
+        }, 10);
+        
+        return newTile;
     }
+    return null;
+}
 
     getTileAt(row, col) {
         if (row < 0 || row >= this.size || col < 0 || col >= this.size) {
