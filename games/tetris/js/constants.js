dfb929432d0b949b3525a1bfd2ef5e38acb15715
Geek Neuron <geekneuron@gmail.com>
2025-06-08 11:32:20 +0330

    Update constants.js

--- games/tetris/js/constants.js
+++ games/tetris/js/constants.js
@@ -1,9 +1,8 @@
-// --- NEW: Control the size of pieces in Hold/Next boxes here ---
+// --- NEW: Independent controls for Width and Height in Hold/Next boxes ---
 // Control the piece's Width (پهنا)
 const SIDE_PANEL_SCALE_X = 2.0; 
 // Control the piece's Height (طول)
-const SIDE_PANEL_SCALE_Y = 2.0;
-
+const SIDE_PANEL_SCALE_Y = 1.0;
 
 const COLS = 10;
 const ROWS = 20;
@@ -11,27 +10,20 @@ const BLOCK_SIZE = 30;
 
 const SHAPES = [
     [], // Empty shape
-    [[1, 1, 1], [0, 1, 0]],                                 // T
-    [[2, 2, 2, 2]],                                        // I
-    [[0, 3, 3], [3, 3, 0]],                                 // S
-    [[4, 4, 0], [0, 4, 4]],                                 // Z
-    [[5, 5, 5], [5, 0, 0]],                                 // L
-    [[6, 6, 6], [0, 0, 6]],                                 // J
-    [[7, 7], [7, 7]]                                       // O
+    [[1, 1, 1], [0, 1, 0]],
+    [[2, 2, 2, 2]],
+    [[0, 3, 3], [3, 3, 0]],
+    [[4, 4, 0], [0, 4, 4]],
+    [[5, 5, 5], [5, 0, 0]],
+    [[6, 6, 6], [0, 0, 6]],
+    [[7, 7], [7, 7]]
 ];
 
 const KEY = {
-    LEFT: 37,
-    RIGHT: 39,
-    DOWN: 40,
-    UP: 38,    // For rotation
-    SPACE: 32, // For hard drop
-    C: 67      // For Hold
+    LEFT: 37, RIGHT: 39, DOWN: 40, UP: 38,
+    SPACE: 32, C: 67
 };
 
 const POINTS = {
-    SINGLE: 100,
-    DOUBLE: 300,
-    TRIPLE: 500,
-    TETRIS: 800
+    SINGLE: 100, DOUBLE: 300, TRIPLE: 500, TETRIS: 800
 };
