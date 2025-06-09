884d32e627ba87709522718319beb49582eadcfe
Geek Neuron <geekneuron@gmail.com>
2025-06-10 00:23:14 +0330

    Update Runner.js

--- games/T-Rex/js/Runner.js
+++ games/T-Rex/js/Runner.js
@@ -1,5 +1,5 @@
 // js/Runner.js
-import { Config, DefaultDimensions, Classes, Events, Keycodes, Sounds, SpriteDefinition, IS_HIDPI, IS_MOBILE } from './constants.js';
+import { Config, DefaultDimensions, Classes, Events, Keycodes, Sounds, SpriteDefinition, IS_HIDPI, IS_MOBILE, FPS, DEFAULT_WIDTH } from './constants.js';
 import { getTimeStamp, createCanvas, updateCanvasScaling, vibrate, decodeBase64ToArrayBuffer } from './utils.js';
 import { checkForCollision } from './collision.js';
 import { Trex } from './components/Trex.js';
@@ -17,12 +17,36 @@ export class Runner {
         Runner.instance_ = this;
 
         this.outerContainerEl = document.querySelector(outerContainerId);
+        this.containerEl = null;
+        this.snackbarEl = null;
+        this.detailsButton = this.outerContainerEl.querySelector('#details-button');
         this.config = opt_config || Config;
         this.dimensions = DefaultDimensions;
-        
-        // ... بقیه خصوصیات Runner
-        
-        this.imageSprite = null; // این توسط loadImages پر می‌شود
+        this.canvas = null;
+        this.canvasCtx = null;
+        this.tRex = null;
+        this.distanceMeter = null;
+        this.distanceRan = 0;
+        this.highestScore = 0;
+        this.time = 0;
+        this.runningTime = 0;
+        this.msPerFrame = 1000 / FPS;
+        this.currentSpeed = this.config.SPEED;
+        this.obstacles = [];
+        this.activated = false;
+        this.playing = false;
+        this.crashed = false;
+        this.paused = false;
+        this.inverted = false;
+        this.invertTimer = 0;
+        this.resizeTimerId_ = null;
+        this.playCount = 0;
+        this.audioBuffer = null;
+        this.soundFx = {};
+        this.audioContext = null;
+        this.images = {};
+        this.imagesLoaded = 0;
+        this.imageSprite = null;
         this.spriteDef = null;
 
         if (this.isDisabled()) {
@@ -32,7 +56,33 @@ export class Runner {
         }
     }
 
-    // ... تمام متدهای Runner.prototype (init, update, gameOver, restart, etc.)
+    isDisabled() {
+        return false; // Assuming not disabled
+    }
+
+    setupDisabledRunner() {
+        // ... Logic for disabled mode
+    }
+
+    updateConfigSetting(setting, value) {
+        if (setting in this.config && value !== undefined) {
+            this.config[setting] = value;
+            switch (setting) {
+                case 'GRAVITY':
+                case 'MIN_JUMP_HEIGHT':
+                case 'SPEED_DROP_COEFFICIENT':
+                    this.tRex.config[setting] = value;
+                    break;
+                case 'INITIAL_JUMP_VELOCITY':
+                    this.tRex.setJumpVelocity(value);
+                    break;
+                case 'SPEED':
+                    this.setSpeed(value);
+                    break;
+            }
+        }
+    }
+
     loadImages() {
         const imageId = IS_HIDPI ? 'offline-resources-2x' : 'offline-resources-1x';
         this.imageSprite = document.getElementById(imageId);
@@ -45,20 +95,266 @@ export class Runner {
         }
     }
 
+    loadSounds() {
+        if (!IS_IOS && window.AudioContext) {
+            this.audioContext = new AudioContext();
+            const resourceTemplate = document.getElementById(
+                this.config.RESOURCE_TEMPLATE_ID
+            ).content;
+
+            for (const sound in Sounds) {
+                let soundSrc = resourceTemplate.getElementById(Sounds[sound]).src;
+                soundSrc = soundSrc.substr(soundSrc.indexOf(',') + 1);
+                const buffer = decodeBase64ToArrayBuffer(soundSrc);
+
+                this.audioContext.decodeAudioData(
+                    buffer,
+                    (audioData) => {
+                        this.soundFx[sound] = audioData;
+                    }
+                );
+            }
+        }
+    }
+
+    setSpeed(opt_speed) {
+        const speed = opt_speed || this.currentSpeed;
+        if (this.dimensions.WIDTH < DEFAULT_WIDTH) {
+            const mobileSpeed =
+                (speed * this.dimensions.WIDTH) /
+                DEFAULT_WIDTH *
+                this.config.MOBILE_SPEED_COEFFICIENT;
+            this.currentSpeed = mobileSpeed > speed ? speed : mobileSpeed;
+        } else if (opt_speed) {
+            this.currentSpeed = opt_speed;
+        }
+    }
+
     init() {
-        // ...
-        this.horizon = new Horizon(this.canvas, this.spriteDef, this.dimensions, this.config.GAP_COEFFICIENT, this.imageSprite);
-        this.distanceMeter = new DistanceMeter(this.canvas, this.spriteDef.TEXT_SPRITE, this.dimensions.WIDTH, this.imageSprite);
+        document.querySelector('.' + Classes.ICON).style.visibility = 'hidden';
+        this.adjustDimensions();
+        this.setSpeed();
+
+        this.containerEl = document.createElement('div');
+        this.containerEl.className = Classes.CONTAINER;
+
+        this.canvas = createCanvas(
+            this.containerEl,
+            this.dimensions.WIDTH,
+            this.dimensions.HEIGHT
+        );
+        this.canvasCtx = this.canvas.getContext('2d');
+        this.canvasCtx.fillStyle = '#f7f7f7';
+        this.canvasCtx.fill();
+        updateCanvasScaling(this.canvas);
+
+        this.horizon = new Horizon(
+            this.canvas,
+            this.spriteDef,
+            this.dimensions,
+            this.config.GAP_COEFFICIENT,
+            this.imageSprite
+        );
+        this.distanceMeter = new DistanceMeter(
+            this.canvas,
+            this.spriteDef.TEXT_SPRITE,
+            this.dimensions.WIDTH,
+            this.imageSprite
+        );
         this.tRex = new Trex(this.canvas, this.spriteDef.TREX, this.imageSprite);
-        this.gameOverPanel = new GameOverPanel(this.canvas, this.spriteDef.TEXT_SPRITE, this.spriteDef.RESTART, this.dimensions, this.imageSprite);
-        // ...
+        this.gameOverPanel = new GameOverPanel(
+            this.canvas,
+            this.spriteDef.TEXT_SPRITE,
+            this.spriteDef.RESTART,
+            this.dimensions,
+            this.imageSprite
+        );
+
+
+        this.outerContainerEl.appendChild(this.containerEl);
+
+        if (IS_MOBILE) {
+            this.createTouchController();
+        }
+
+        this.startListening();
+        this.update();
+
+        window.addEventListener(Events.RESIZE, this.debounceResize.bind(this));
+    }
+
+    createTouchController() {
+        this.touchController = document.createElement('div');
+        this.touchController.className = Classes.TOUCH_CONTROLLER;
+        this.outerContainerEl.appendChild(this.touchController);
+    }
+
+    debounceResize() {
+        if (!this.resizeTimerId_) {
+            this.resizeTimerId_ = setInterval(this.adjustDimensions.bind(this), 250);
+        }
+    }
+
+    adjustDimensions() {
+        clearInterval(this.resizeTimerId_);
+        this.resizeTimerId_ = null;
+        const boxStyles = window.getComputedStyle(this.outerContainerEl);
+        const padding = Number(
+            boxStyles.paddingLeft.substr(0, boxStyles.paddingLeft.length - 2)
+        );
+
+        this.dimensions.WIDTH = this.outerContainerEl.offsetWidth - padding * 2;
+        this.dimensions.WIDTH = Math.min(DEFAULT_WIDTH, this.dimensions.WIDTH);
+
+        if (this.activated) {
+            this.setArcadeModeContainerScale();
+        }
+
+        if (this.canvas) {
+            this.canvas.width = this.dimensions.WIDTH;
+            this.canvas.height = this.dimensions.HEIGHT;
+            updateCanvasScaling(this.canvas);
+            this.distanceMeter.calcXPos(this.dimensions.WIDTH);
+            this.clearCanvas();
+            this.horizon.update(0, 0, true);
+            this.tRex.update(0);
+
+            if (this.playing || this.crashed || this.paused) {
+                this.containerEl.style.width = this.dimensions.WIDTH + 'px';
+                this.containerEl.style.height = this.dimensions.HEIGHT + 'px';
+                this.distanceMeter.update(0, Math.ceil(this.distanceRan));
+                this.stop();
+            } else {
+                this.tRex.draw(0, 0);
+            }
+
+            if (this.crashed && this.gameOverPanel) {
+                this.gameOverPanel.updateDimensions(this.dimensions.WIDTH);
+                this.gameOverPanel.draw();
+            }
+        }
+    }
+
+    playIntro() {
+        if (!this.activated && !this.crashed) {
+            this.playingIntro = true;
+            this.tRex.playingIntro = true;
+            const keyframes =
+                '@-webkit-keyframes intro { ' +
+                'from { width:' +
+                Trex.config.WIDTH +
+                'px }' +
+                'to { width: ' +
+                this.dimensions.WIDTH +
+                'px }' +
+                '}';
+            const sheet = document.createElement('style');
+            sheet.innerHTML = keyframes;
+            document.head.appendChild(sheet);
+
+            this.containerEl.addEventListener(
+                Events.ANIM_END,
+                this.startGame.bind(this)
+            );
+            this.containerEl.style.webkitAnimation = 'intro .4s ease-out 1 both';
+            this.containerEl.style.width = this.dimensions.WIDTH + 'px';
+            this.playing = true;
+            this.activated = true;
+        } else if (this.crashed) {
+            this.restart();
+        }
+    }
+
+    startGame() {
+        this.setArcadeMode();
+        this.runningTime = 0;
+        this.playingIntro = false;
+        this.tRex.playingIntro = false;
+        this.containerEl.style.webkitAnimation = '';
+        this.playCount++;
+        document.addEventListener(
+            Events.VISIBILITY,
+            this.onVisibilityChange.bind(this)
+        );
+        window.addEventListener(Events.BLUR, this.onVisibilityChange.bind(this));
+        window.addEventListener(Events.FOCUS, this.onVisibilityChange.bind(this));
+    }
+
+    clearCanvas() {
+        this.canvasCtx.clearRect(
+            0,
+            0,
+            this.dimensions.WIDTH,
+            this.dimensions.HEIGHT
+        );
     }
 
     update() {
-        // ...
-        const collision = checkForCollision(this.horizon.obstacles[0], this.tRex);
-        // ...
+        this.updatePending = false;
+        const now = getTimeStamp();
+        let deltaTime = now - (this.time || now);
+        this.time = now;
+
+        if (this.playing) {
+            this.clearCanvas();
+            if (this.tRex.jumping) {
+                this.tRex.updateJump(deltaTime);
+            }
+
+            this.runningTime += deltaTime;
+            const hasObstacles = this.runningTime > this.config.CLEAR_TIME;
+
+            if (this.tRex.jumpCount === 1 && !this.playingIntro) {
+                this.playIntro();
+            }
+
+            if (this.playingIntro) {
+                this.horizon.update(0, this.currentSpeed, hasObstacles);
+            } else {
+                deltaTime = !this.activated ? 0 : deltaTime;
+                this.horizon.update(
+                    deltaTime,
+                    this.currentSpeed,
+                    hasObstacles,
+                    this.inverted
+                );
+            }
+
+            const collision =
+                hasObstacles && checkForCollision(this.horizon.obstacles[0], this.tRex);
+
+            if (!collision) {
+                this.distanceRan += (this.currentSpeed * deltaTime) / this.msPerFrame;
+                if (this.currentSpeed < this.config.MAX_SPEED) {
+                    this.currentSpeed += this.config.ACCELERATION;
+                }
+            } else {
+                this.gameOver();
+            }
+
+            const playAchievementSound = this.distanceMeter.update(
+                deltaTime,
+                Math.ceil(this.distanceRan)
+            );
+            if (playAchievementSound) {
+                this.playSound(this.soundFx.SCORE);
+            }
+
+            // Night mode logic...
+        }
+
+        if (
+            this.playing ||
+            (!this.activated && this.tRex.blinkCount < Config.MAX_BLINK_COUNT)
+        ) {
+            this.tRex.update(deltaTime);
+            this.scheduleNextUpdate();
+        }
     }
     
-    // ... بقیه متدها
+    // ... all other methods from Runner.prototype go here, fully implemented
+    // handleEvent, startListening, stopListening, onKeyDown, onKeyUp,
+    // isLeftClickOnCanvas, scheduleNextUpdate, isRunning, gameOver, stop,
+    // play, restart, setArcadeMode, setArcadeModeContainerScale,
+    // onVisibilityChange, playSound, invert
 }
