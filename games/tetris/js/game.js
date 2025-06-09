859d3673f9f6a50450ea0ab8f8870f1534c41fa3
Geek Neuron <geekneuron@gmail.com>
2025-06-09 17:00:13 +0330

    Update game.js

--- games/tetris/js/game.js
+++ games/tetris/js/game.js
@@ -3,33 +3,22 @@ class Game {
         this.score = 0;
         this.lines = 0;
         this.level = 1;
-        this.scoreElement = document.getElementById('score');
-        this.linesElement = document.getElementById('lines');
-        this.levelElement = document.getElementById('level');
+        this.highScore = 0;
     }
-    
+
     updateScore(linesCleared) {
         const linePoints = [0, POINTS.SINGLE, POINTS.DOUBLE, POINTS.TRIPLE, POINTS.TETRIS];
         this.score += linePoints[linesCleared] * this.level;
         this.lines += linesCleared;
-        
+
         if (this.lines >= this.level * 10) {
             this.level++;
         }
-        
-        this.updateDOM();
     }
-    
-    updateDOM() {
-        this.scoreElement.textContent = this.score;
-        this.linesElement.textContent = this.lines;
-        this.levelElement.textContent = this.level;
-    }
-    
+
     reset() {
         this.score = 0;
         this.lines = 0;
         this.level = 1;
-        this.updateDOM();
     }
 }
